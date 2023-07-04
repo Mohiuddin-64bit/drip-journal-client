6d024f0ae1fa804d6c7b1f79ab04bfaaf99bb212
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-07-04 13:27:29 +0600

    all user's page done

--- src/component/Admin/Admin.jsx
+++ src/component/Admin/Admin.jsx
@@ -1,9 +1,53 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Admin = () => {
+  const [information, setInformation] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/allInformation")
+      .then((res) => {
+        setInformation(res.data);
+      })
+      .catch((error) => console.log(error.message));
+  }, []);
+  console.log(information);
   return (
     <div>
-      
+      <img
+        className="w-64 mx-auto mt-12 mb-12"
+        src={`https://i.ibb.co/wSbpCDJ/Logo.png`}
+        alt=""
+      />
+      <h2 className="text-center font-bold text-2xl underline mb-2">
+        All Subscriber
+      </h2>
+      <div className="overflow-x-auto w-full mx-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead className="text-center">
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            {/* <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+            </tr> */}
+            {information.map((info, index) => (
+              <tr key={info._id}>
+                <th>{index + 1}</th>
+                <td>{info.name}</td>
+                <td>{info.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Admin from './component/Admin/Admin.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Admin from "./component/Admin/Admin.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
   },
+
   {
-    path: "admin",
+    path: "/admin",
     element: <Admin></Admin>,
-  },
+  }
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
